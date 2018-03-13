997ffb1646bc4c7ecdc779ec084feb220f40d017
gioboa <giorgiob.boa@gmail.com>
2018-03-13 15:46:57 +0100

    add http request

--- server.ts
+++ server.ts
@@ -1,16 +1,24 @@
+// **Server-Side Rendering(ssr)**
+// * Happens at runtime
+// * Uses `ngExpressEngine` to render your application on the fly at the requested url.
+
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
-import {join} from 'path';
-import {readFileSync} from 'fs';
+import * as bodyParser from 'body-parser';
+
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
 // Express server
 const app = express();
+const json_body_parser = bodyParser.json();
+const urlencoded_body_parser = bodyParser.urlencoded({ extended: true });
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
@@ -19,20 +27,24 @@ const DIST_FOLDER = join(process.cwd(), 'dist');
 const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main.bundle');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
 
 // Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine(
+  'html',
+  ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)]
+  })
+);
+
+app.use(json_body_parser);
+app.use(urlencoded_body_parser);
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
@@ -42,13 +54,21 @@ app.set('views', join(DIST_FOLDER, 'browser'));
 */
 
 // Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
-  maxAge: '1y'
-}));
+app.get(
+  '*.*',
+  express.static(join(DIST_FOLDER, 'browser'), {
+    maxAge: '1y'
+  })
+);
 
 // ALl regular routes use the Universal engine
 app.get('*', (req, res) => {
   res.render('index', { req });
+});
+
+app.post('/users', function(req, res) {
+  console.log(`Server log - HTTP users`, req.body);
+  res.status(200).send({ users: ['A', 'B', 'C'] });
 });
 
 // Start up the Node server
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-home',
-  template: `<h3>{{ message }}</h3>`
+  template: `<h3>{{ users | json  }}</h3><button (click)="fetchData()">Fetch data</button>`
 })
-export class HomeComponent implements OnInit {
-  public message: string;
+export class HomeComponent {
+  public users: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.message = 'Hello';
+  public fetchData() {
+    const headersDefault: HttpHeaders = new HttpHeaders({
+      Accept: 'application/json; charset=UTF-8',
+      'Cache-Control': 'no-cache',
+      'Cache-control': 'no-store',
+      Expires: '0',
+      Pragma: 'no-cache',
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Require-Content-Type': 'application/json',
+      timeout: `60000`
+    });
+    // Merge default headers with new headers
+    //
+    const headerRequest: HttpHeaders = headersDefault;
+    this.http.post('http://localhost:4000/users', { name: 'some-data' }, { headers: headerRequest }).subscribe(data => {
+      console.log(data);
+      this.users = data;
+    });
   }
 }
