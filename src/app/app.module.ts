c94df5f7ad35877d77bff08d8d6b5dee45826bb1
gioboa <giorgiob.boa@gmail.com>
2018-03-13 12:16:15 +0100

    setup SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import {TransferHttpCacheModule} from '@nguniversal/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'my-app'}),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+    ]),
+    TransferHttpCacheModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
